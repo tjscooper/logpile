9dd46bbb885d52a6e883909fd180e50d940c0ee6
Tim Cooper <tcooper@weedmaps.com>
2019-04-16 01:20:54 -0400

    add new pub and method for updating last log

--- imports/api/logs.js
+++ imports/api/logs.js
@@ -5,16 +5,21 @@ import LogService from '../service/log-service.js';
 
 if (Meteor.isServer) {
   Meteor.publish('logs', logsPublication = () => {
-    return LogsCollection.find({}, { sort: { createdAt: -1 } });
+    return LogsCollection.find({}, { sort: { timerStart: 1 } });
   });
 
   Meteor.publish('log', logsPublication = ({ logId }) => {
     return LogsCollection.find(logId);
+  });
+
+  Meteor.publish('log.last', logsPublication = () => {
+    return LogsCollection.find({}, { sort: { timerStart: -1 }, limit: 1 });
   });
 }
 
 Meteor.methods({
   'logs.insert'(model) { return LogService.insert(model); },
   'logs.update'(model) { return LogService.update(model); },
+  'logs.update.last'(model) { return LogService.updateLastLog(model); },
   'logs.remove'(model) { return LogService.remove(model); },
 });
